633e604dcc697ae4e3b217cf3a03eaae00cb3dcf
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-15 23:53:05 -0700

    Tick

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ class develop(_develop):
         _develop.run(self)
 
 setup(
-    name='shellphish-afl', version='0.7', description="pip package for afl",
+    name='shellphish-afl', version='0.8', description="pip package for afl",
     packages=['shellphish_afl'],
     cmdclass={'build': build, 'develop': develop},
     data_files=data_files,
